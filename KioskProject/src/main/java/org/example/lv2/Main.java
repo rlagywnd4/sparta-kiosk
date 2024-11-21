575c21b78f32bb575dd197985a8f0a69e4f687f7
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-21 14:31:26 +0900

    feat: MenuItem class
    MenuItem class생성
    main에 MenuItem 반영

--- KioskProject/src/main/java/org/example/lv2/Main.java
+++ KioskProject/src/main/java/org/example/lv2/Main.java
@@ -1,37 +1,41 @@
 package org.example.lv2;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> menus = new ArrayList<>();
-        menus.add("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        menus.add("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        menus.add("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        menus.add("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         boolean isRunning = true;
         while(isRunning){
             System.out.println("[ SHAKESHACK MENU ]");
 
-            menus.forEach(System.out::println);
-            System.out.println("0. 종료      | 종료");
+            // print menu
+            for(int i = 0; i < menuItems.size(); i++){
+                System.out.println((i+1)+". "+menuItems.get(i).name+ "\t| W "+menuItems.get(i).price+" | "+menuItems.get(i).descriptrion);
+            }
+            System.out.println("0. 종료\t\t| 종료");
 
             switch (scanner.next()){
                 case "1":
-                    System.out.println(menus.get(0));
+                    System.out.println("1. "+menuItems.get(0).name+ "\t| W "+menuItems.get(0).price+" | "+menuItems.get(0).descriptrion);
                     break;
                 case "2":
-                    System.out.println(menus.get(1));
+                    System.out.println("2. "+menuItems.get(1).name+ "\t| W "+menuItems.get(1).price+" | "+menuItems.get(1).descriptrion);
                     break;
                 case "3":
-                    System.out.println(menus.get(2));
+                    System.out.println("3. "+menuItems.get(2).name+ "\t| W "+menuItems.get(2).price+" | "+menuItems.get(2).descriptrion);
                     break;
                 case "4":
-                    System.out.println(menus.get(3));
+                    System.out.println("4. "+menuItems.get(3).name+ "\t| W "+menuItems.get(3).price+" | "+menuItems.get(3).descriptrion);
                     break;
                 case "0":
                     isRunning = false;
@@ -39,6 +43,7 @@ public class Main {
                 default:
                     System.out.println("잘못입력하셨습니다.");
             }
+            System.out.println();
         }
 
     }
---
+++ KioskProject/src/main/java/org/example/lv2/MenuItem.java
@@ -0,0 +1,13 @@
+package org.example.lv2;
+
+public class MenuItem {
+    String name;
+    double price;
+    String descriptrion;
+
+    MenuItem(String name, double price, String descriptrion){
+        this.name = name;
+        this.price = price;
+        this.descriptrion = descriptrion;
+    }
+}
