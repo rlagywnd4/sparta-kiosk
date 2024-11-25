f7628672755bfe5949b6f8ea96aba69b6af207e1
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-25 16:53:17 +0900

    fix: MenuItem comment
    클래스 설명 간소화

--- KioskProject/src/main/java/org/example/lv4/MenuItem.java
+++ KioskProject/src/main/java/org/example/lv4/MenuItem.java
@@ -1,16 +1,14 @@
 package org.example.lv4;
 
 /**
- * MenuItem
- * 메뉴의 속성을 정의하는 클래스입니다.
- * 이름, 가격, 설명을 저장할 수 있습니다.
+ * 메뉴의 속성을 정의하는 클래스
  */
 public class MenuItem {
     String name;
     double price;
     String description;
 
-    MenuItem(String name, double price, String description){
+    MenuItem(String name, double price, String description) {
         this.name = name;
         this.price = price;
         this.description = description;
