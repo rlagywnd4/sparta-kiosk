9ea4fa4c2473f38cd9e34d771657030ac21bc338
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-25 16:49:44 +0900

    feat: print category
    카테고리 출력 기능 추가
    메뉴 아이템의 개수에 대응할 수 있도록 변경

--- KioskProject/src/main/java/org/example/lv4/Kiosk.java
+++ KioskProject/src/main/java/org/example/lv4/Kiosk.java
@@ -4,51 +4,44 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- *  키오스크 클래스
- *  menuItem을 관리하고 사용자 입력을 처리하는 클래스입니다.
+ * menuItem을 관리하고 사용자 입력을 처리하는 클래스
  */
 public class Kiosk {
-    List<MenuItem> menuItems;
-
-    public Kiosk(List<MenuItem> menuItems) {
-        this.menuItems = menuItems;
-    }
-
     /**
-     * start()
-     * 사용자 입력을 받고 메뉴를 출력하는 키오스크 기능을 실행하는 메소드입니다.
+     * 사용자 입력을 받고 메뉴를 출력하는 키오스크 기능을 실행
      */
-    public void start(){
+    public void start(List<Menu> menu) {
         Scanner scanner = new Scanner(System.in);
 
         boolean isRunning = true;
-        while(isRunning){
+        while (isRunning) {
             System.out.println("[ SHAKESHACK MENU ]");
 
-            // print menu
-            for(int i = 0; i < menuItems.size(); i++){
-                System.out.println((i+1)+". "+menuItems.get(i).name+ "\t| W "+menuItems.get(i).price+" | "+menuItems.get(i).description);
+            for (int i = 0; i < menu.size(); i++) {
+                System.out.println((i + 1) + ". " + menu.get(i).getCategory());
             }
             System.out.println("0. 종료\t\t| 종료");
 
-            switch (scanner.next()){
-                case "1":
-                    System.out.println("1. "+menuItems.get(0).name+ "\t| W "+menuItems.get(0).price+" | "+menuItems.get(0).description);
-                    break;
-                case "2":
-                    System.out.println("2. "+menuItems.get(1).name+ "\t| W "+menuItems.get(1).price+" | "+menuItems.get(1).description);
-                    break;
-                case "3":
-                    System.out.println("3. "+menuItems.get(2).name+ "\t| W "+menuItems.get(2).price+" | "+menuItems.get(2).description);
-                    break;
-                case "4":
-                    System.out.println("4. "+menuItems.get(3).name+ "\t| W "+menuItems.get(3).price+" | "+menuItems.get(3).description);
-                    break;
-                case "0":
-                    isRunning = false;
-                    break;
-                default:
-                    System.out.println("잘못입력하셨습니다.");
+            int category = scanner.nextInt();
+
+            if (0 < category && category <= menu.size()) {
+                menu.get(category - 1).printMenuItems();
+
+                System.out.println("0. 뒤로가기");
+            } else if (category == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("잘못된 입력입니다.");
+                continue;
+            }
+
+            int menuItem = scanner.nextInt();
+            if (0 < menuItem && menuItem <= menu.get(category - 1).menuItems.size()) {
+                System.out.println("선택한 메뉴: " + menu.get(category - 1).menuItems.get(menuItem - 1).name + "\t| W " + menu.get(category - 1).menuItems.get(menuItem - 1).price + " | " + menu.get(category - 1).menuItems.get(menuItem - 1).description);
+            } else if (menuItem == 0) {
+            } else {
+                System.out.println("잘못된 입력입니다.");
             }
             System.out.println();
         }
