6122593ffde401db3b25c5d00d8923b766f10d72
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-25 16:47:43 +0900

    feat: Menu class
    MenuItem을 관리하는 클래스

---
+++ KioskProject/src/main/java/org/example/lv4/Menu.java
@@ -0,0 +1,29 @@
+package org.example.lv4;
+
+import java.util.List;
+
+/**
+ * MenuItem 클래스를 관리하는 클래스입니다.
+ */
+public class Menu {
+    List<MenuItem> menuItems;
+    String category;
+
+    public Menu(String category) {
+        this.category = category;
+    }
+
+    String getCategory() {
+        return this.category;
+    }
+
+    /**
+     * MenuItem을 순차적으로 출력
+     */
+    public void printMenuItems() {
+        System.out.println("[ " + this.category.toUpperCase() + " MENU ]");
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println((i + 1) + ". " + menuItems.get(i).name + "\t| W " + menuItems.get(i).price + " | " + menuItems.get(i).description);
+        }
+    }
+}
